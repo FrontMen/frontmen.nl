5452cf2a25bed14c969685c5ea8004e5a7c179e0
Sander de Jong <sander@frontmen.nl>
2018-08-14 08:45:52 +0200

    Improve imagemin configuration

--- scripts/assets/asset.js
+++ scripts/assets/asset.js
@@ -12,7 +12,17 @@ function copyAsset(file, { srcPath, outputPath }) {
   const destPath = path.join(outputPath, fileData.dir).replace('/pages', '/');
 
   return imagemin([path.join(srcPath, file)], destPath, {
-    plugins: [imageminJpegtran(), imageminPngquant({ quality: '75' }), imageminSvgo()]
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant({
+        quality: '75'
+      }),
+      imageminSvgo({
+        plugins: [
+          {removeViewBox: false},
+          {removeDimensions: true }
+        ]
+      })]
   });
 }
 
