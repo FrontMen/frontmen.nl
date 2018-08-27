8edb548ef65e3fe3c8cc264c07b24e79b30d66da
Sander de Jong <sander@frontmen.nl>
2018-08-27 23:37:25 +0200

    Rename folders

--- scripts/pages/layout.js
+++ scripts/pages/layout.js
@@ -4,7 +4,7 @@ import fse from 'fs-extra';
  * Loads a layout file
  */
 const loadLayout = (layout, { srcPath }) => {
-  const file = `${srcPath}/layouts/${layout}.ejs`;
+  const file = `${srcPath}/_layouts/${layout}.ejs`;
   const data = fse.readFileSync(file, 'utf-8');
 
   return { file, data };
