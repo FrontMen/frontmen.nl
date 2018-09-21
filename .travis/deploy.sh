6f86dec38fecbc962b8ff8cada18654e0b668a37
LC <lassecapel@gmail.com>
2018-09-21 09:53:12 +0200

    make travis execute deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 set -e
-ssh <user>@<host.tld> [-p port] -v exit
+
+PROJECT="frontmen"
+DIR_GIT="/var/repo/"
+USER="git"
+LIVE_IP="149.210.172.104"
+
+
+ssh ${USER}@${LIVE_IP} -v exit
+
+git config --global push.default simple # we only want to push one branch — master
+# specify the repo on the live server as a remote repo, and name it 'production'
+# <user> here is the separate user you created for deploying
+git remote add live ssh://${USER}@${LIVE_IP}${DIR_GIT}${PROJECT}.git
+git push live master # push our updates
