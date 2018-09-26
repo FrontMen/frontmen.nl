b7841f427ba5cff46d6faa9aa58666de8e0847bc
LC <lassecapel@gmail.com>
2018-09-26 08:24:12 +0200

    mutate gitignore on build server to be able to deploy dist folder

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -8,14 +8,12 @@ LIVE_IP="149.210.172.104"
 
 ssh ${USER}@${LIVE_IP} -v exit
 
-cd dist
-
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
 git remote add live ssh://${USER}@${LIVE_IP}${DIR_GIT}${PROJECT}.git
 
-git add *
+git add dist
 git commit -am "Travis CI"
 
 git push live master # push our updates
