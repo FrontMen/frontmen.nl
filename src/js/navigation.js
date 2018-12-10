1d674baa5c671c0343e73280ee17ae93ba32a300
kevinegstorf <kevin.egstorf@gmail.com>
2018-12-10 13:25:05 +0100

    fix on nav opening and closing

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,6 +2,8 @@ const layout = document.querySelector('.o-layout');
 const toggleButton = document.querySelector('.js-menu-toggle');
 const toggleButtonIcon = toggleButton.querySelector('.c-icon');
 const navigationClass = '.o-layout__navigation';
+const item = document.querySelector('.c-page-navigation__list ');
+const link = document.querySelector('.c-page-navigation__link');
 
 const toggleMenu = () => {
   layout.classList.toggle('o-layout--open');
@@ -9,8 +11,24 @@ const toggleMenu = () => {
   toggleButtonIcon.classList.toggle('c-icon--menu-close');
 };
 
+const closeMenu = () => {
+  layout.classList.remove('o-layout--open');
+};
+
 const closeOnClickOutside = selector => {
   const outsideClickListener = event => {
+    if (event.target.closest(selector) === item || link) {
+      event.preventDefault();
+
+      if (event.target.innerHTML === 'Work') {
+        return '/';
+      }
+      closeMenu();
+
+      setTimeout(() => {
+        window.location.href = event.target.href;
+      }, 300);
+    }
     if (event.target.closest(selector) === null) {
       toggleMenu();
       document.removeEventListener('click', outsideClickListener);
